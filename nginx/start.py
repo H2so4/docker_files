f54e2026f527f58621e08bbaac07f27abaa74729
OkezieE <okezie@gmail.com>
2018-01-31 02:18:02 -0800

    Use the .get method instead
    Checking for environment variables using os.environ['EnvVarName'] will fail if the environment variable does not exist.  The .get method will return None if the key doesn't exist, which gives the desired result.
    ```
    Traceback (most recent call last):
      File "/start.py", line 11, in <module>
        if not os.environ["SOCKET_PORT"]:
      File "/usr/lib/python2.7/UserDict.py", line 40, in __getitem__
        raise KeyError(key)
    KeyError: 'SOCKET_PORT'
    ```

--- nginx/start.py
+++ nginx/start.py
@@ -4,20 +4,20 @@ import os
 import subprocess
 
 #Set default port
-if not os.environ["PORT"]:
+if not os.environ.get("PORT"):
 	os.environ["PORT"] = "5000"
 
 #Set default sockets port
-if not os.environ["SOCKET_PORT"]:
+if not os.environ.get("SOCKET_PORT"):
 	os.environ["SOCKET_PORT"] = "20523"
 
 # Actual startup script
-if not os.path.exists("/certs/dhparam.pem") and os.environ["TLS_FLAVOR"] != "notls":
+if not os.path.exists("/certs/dhparam.pem") and os.environ.get("TLS_FLAVOR") != "notls":
     os.system("openssl dhparam -out /certs/dhparam.pem 2048")
 
-if os.environ["TLS_FLAVOR"] == "letsencrypt":
+if os.environ.get("TLS_FLAVOR") == "letsencrypt":
     subprocess.Popen(["/letsencrypt.py"])
-elif os.environ["TLS_FLAVOR"] == "cert":
+elif os.environ.get("TLS_FLAVOR") == "cert":
 	if not os.path.exists("/certs/cert.pem"):
 		os.system("openssl req -newkey rsa:2048 -x509 -keyout /certs/key.pem -out /certs/cert.pem -days 365 -nodes -subj '/C=NA/ST=None/L=None/O=None/CN=" + os.environ["BASE_URL"] + "'")
 
